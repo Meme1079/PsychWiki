8e02fd4d59043cdefc34a79fcd69ff6f42db8ae8
Meme1079 <101881784+Meme1079@users.noreply.github.com>
2022-08-06 23:04:33 +0800

    Delete ExampleScript/Basic directory

--- ExampleScript/Basic/GameOverScript.lua
+++
@@ -1,7 +0,0 @@
-function onCreate()
-     addCharacterToList('bf-dead', 'boyfriend');
-     setPropertyFromClass('GameOverSubstate', 'characterName', 'bf-dead') -- put the character name
-     setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx'); -- put the sound when you die
-     setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOverEnd'); -- put the music when it's looping
-     setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameover2'); -- put the sound when you press return
-end
